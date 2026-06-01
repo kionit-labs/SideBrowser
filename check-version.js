2213cff6fdbc6f0802c5f8c20f7f1a284bc1036d
Ismail Sahin <ismailsahin260@gmail.com>
2026-06-01 16:07:53 +0200

    chore: remove obsolete diagnostic scripts and redundant assets banner

--- check-version.js
+++
@@ -1,6 +0,0 @@
-const { app } = require('electron');
-app.whenReady().then(() => {
-  console.log('Electron Version:', process.versions.electron);
-  console.log('Chrome Version:', process.versions.chrome);
-  process.exit(0);
-});
--- check-win-ses.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-app.whenReady().then(() => {
-  const win = new BrowserWindow({ show: false });
-  const ses = win.webContents.session;
-  console.log('--- WINDOW SESSION INFO ---');
-  console.log('Has registerPreloadScript:', typeof ses.registerPreloadScript);
-  console.log('Has setPreloads:', typeof ses.setPreloads);
-  process.exit(0);
-});
